09e44620a850eab0458a4da57573cb48c7130c75
Tanisha <vermatanisha620@gmail.com>
2026-04-26 20:25:21 +0530

    update href link

--- common file/footer.php
+++ common file/footer.php
@@ -61,7 +61,7 @@
             </li>
              <li>
               <i class="bi bi-chevron-right"></i>
-              <a href="https://nestforyou.in/buy/gurgaon/index.php">Blogs</a>
+              <a href="https://nestforyou.in/real-estate/rent/gurgaon/index.php">Blogs</a>
             </li>
           </ul>
         </div>
